baaca8899b85fd58df99eb40e19b8157d5f2481e
mouhamedalger <154691398+mouhamedalger@users.noreply.github.com>
2024-09-02 20:40:41 +0100

    Create سلوت.js

---
+++ scripts/cmds/سلوت.js
@@ -0,0 +1,58 @@
+module.exports.config = {
+		name: "سلوت",
+		version: "1.3",
+		author: "NTKhang",
+		countDown: 5,
+		role: 1,
+		description: {
+			ar: "لعبة تراهن فيها بمبلغ، إذا فزت يتضاعف المبلغ، وإذا خسرت يخصم منك المبلغ الذي راهنت به"
+		},
+		category: "box chat",
+		guide: {
+			ar: " {pn} [المبلغ]"
+		}
+};
+
+module.exports.langs = {
+		ar: {
+        missingInput: "✨ يجب أن تدخل مبلغا للرهان",
+        moneyBetNotEnough: "✨ المال الذي أدخلته للرهان\nبه أكبر من رصيدك 🙄",
+        limitBet: "✨ المال الذي أدخلته للرهان به\nقليل، راهن بأكثر من: 10$ 🙄",
+        returnWin: "✨—-— لقد فزت 😍 —-—✨\n\n ا[ 🎰  %1  |  %2  |  %3  🎰 ]ا\n\n💙 فزت وتمت مضاعفة المبلغ\nالذي راهنت به وأصبح: %4$",
+        returnLose: "✨- بووو لقد خسرت 🤣 -✨\n\nا[ 🎰  %1  |  %2  |  %3  🎰 ]ا\n\n💙 خسرت وتم خصم المبلغ\nالذي راهنت به منك: %4$"
+		}
+	},
+
+module.exports.onStart = async function({ api, event, args, Currencies, getLang }) {
+    const { threadID, messageID, senderID } = event;
+    const { getData, increaseMoney, decreaseMoney } = Currencies;
+    const slotItems = ["🍇", "🍉", "🍊", "🍏", "7⃣", "🍓", "🍒", "🍌", "🥝", "🥑", "🌽"];
+    const moneyUser = (await getData(senderID)).money;
+
+    var moneyBet = parseInt(args[0]);
+    if (isNaN(moneyBet) || moneyBet <= 0) return api.sendMessage(getLang("missingInput"), threadID, messageID);
+	if (moneyBet > moneyUser) return api.sendMessage(getLang("moneyBetNotEnough"), threadID, messageID);
+	if (moneyBet < 10) return api.sendMessage(getLang("limitBet"), threadID, messageID);
+    var number = [], win = false;
+    for (i = 0; i < 3; i++) number[i] = Math.floor(Math.random() * slotItems.length);
+    if (number[0] == number[1] && number[1] == number[2]) {
+        moneyBet *= 9;
+        win = true;
+    }
+    else if (number[0] == number[1] || number[0] == number[2] || number[1] == number[2]) {
+        moneyBet *= 2;
+        win = true;
+    }
+    switch (win) {
+        case true: {
+            api.sendMessage(getLang("returnWin", slotItems[number[0]], slotItems[number[1]], slotItems[number[2]], moneyBet), threadID, messageID);
+            await increaseMoney(senderID, moneyBet);
+            break;
+        }
+        case false: {
+            api.sendMessage(getLang("returnLose", slotItems[number[0]], slotItems[number[1]], slotItems[number[2]], moneyBet), threadID, messageID);
+            await decreaseMoney(senderID, moneyBet);
+            break;
+        }
+    }
+}
