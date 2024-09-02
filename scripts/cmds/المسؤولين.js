188bad11547805b09040314a39c342558be78049
mouhamedalger <154691398+mouhamedalger@users.noreply.github.com>
2024-09-02 07:14:49 +0100

    المسؤولين.js

---
+++ scripts/cmds/المسؤولين.js
@@ -0,0 +1,34 @@
+module.exports.config = {
+		name: "المسؤولين",
+		version: "1.4",
+		author: "محمد تانجيرو",
+		countDown: 5,
+		role: 0,
+		description: {
+			ar: "ترسل لك قائمة المسؤولين في هذا الغروب"
+		},
+		category: "box chat",
+		guide: {
+			ar: "{pn}"
+		}
+	},
+
+module.exports.onStart = async function({ api, event, args, Users }) {
+    var threadInfo = await api.getThreadInfo(event.threadID);
+    let qtv = threadInfo.adminIDs.length;
+    var listad = '';
+    var qtv2 = threadInfo.adminIDs;
+    var fs = global.nodemodule["fs-extra"];
+    dem = 1;
+    for (let i = 0; i < qtv2.length; i++) {
+        const info = (await api.getUserInfo(qtv2[i].id));
+        const name = info[qtv2[i].id].name;
+        listad += '' + `${dem++}` + '. ' + name + '\n';
+    }
+
+    api.sendMessage(
+        `✨ عدد المسؤولين: ${qtv} ✨\n\n 🌹أسماء المسؤولين: 🌹\n${listad}`,
+        event.threadID,
+        event.messageID
+    );
+};
