29badc500e96c2570a1d9a8e181e4aa8316aba60
mouhamedalger <154691398+mouhamedalger@users.noreply.github.com>
2024-09-03 10:34:53 +0100

    Create ضحكات.js

---
+++ scripts/cmds/ضحكات.js
@@ -0,0 +1,71 @@
+const fs = require("fs");
+module.exports.config = {
+    name: "ضحكات",
+		version: "1.3",
+		author: "محمد تانجيرو",
+		countDown: 5,
+		role: 0,
+		description: {
+			ar: "ترسل ضحكات بعض الشخصيات الأسطورية"
+		},
+		category: "no prefix",
+		guide: {
+			ar: "   {pn} [اسم الشخصية]"
+		}
+};
+
+module.exports.onEvent = function({ api, event, client, __GLOBAL }) {
+  var { threadID, messageID } = event;
+  let react = event.body.toLowerCase();
+
+  if(react.includes("ضحكة لايت") || react.includes("ضحكة كيرا")) {
+    var sad = {body: "ضحكة أسطورية 🫣🔥",
+    attachment: fs.createReadStream(__dirname + "/Laughs/kira.mp3")}
+    api.sendMessage(sad, threadID, messageID);
+    api.setMessageReaction("😍", event.messageID, (err) => {}, true)
+        };
+  
+  if(react.includes("ضحكة دوفلامينغو") || react.includes("ضحكة دوفي")) {
+    var sad = {body: "ضحكة أسطورية 🫣🔥",
+    attachment: fs.createReadStream(__dirname + "/Laughs/dofi.mp3")}
+    api.sendMessage(sad, threadID, messageID);
+    api.setMessageReaction("😍", event.messageID, (err) => {}, true)
+        };
+
+  if(react.includes("ضحكة تيتش") || react.includes("ضحكة اللحية السوداء")) {
+    var sad = {body: "ضحكة أسطورية 🫣🔥",
+    attachment: fs.createReadStream(__dirname + "/Laughs/titch.mp3")}
+    api.sendMessage(sad, threadID, messageID);
+    api.setMessageReaction("😍", event.messageID, (err) => {}, true)
+        };
+  
+  if(react.includes("ضحكة كروكودايل") || react.includes("ضحكة كروكو")) {
+    var sad = {body: "ضحكة أسطورية 🫣🔥",
+    attachment: fs.createReadStream(__dirname + "/Laughs/croco.mp3")}
+    api.sendMessage(sad, threadID, messageID);
+    api.setMessageReaction("😍", event.messageID, (err) => {}, true)
+        };
+
+  if(react.includes("ضحكة بروك")) {
+    var sad = {body: "ضحكة أسطورية 🫣🔥",
+    attachment: fs.createReadStream(__dirname + "/Laughs/brook.mp3")}
+    api.sendMessage(sad, threadID, messageID);
+    api.setMessageReaction("😍", event.messageID, (err) => {}, true)
+        };
+
+  if(react.includes("ضحكة سيزار")) {
+    var sad = {body: "ضحكة أسطورية 🫣🔥",
+    attachment: fs.createReadStream(__dirname + "/Laughs/caesar.mp3")}
+    api.sendMessage(sad, threadID, messageID);
+    api.setMessageReaction("😍", event.messageID, (err) => {}, true)
+        };
+
+  if(react.includes("ضحكة مادارا")) {
+    var sad = {body: "ضحكة أسطورية 🫣🔥",
+    attachment: fs.createReadStream(__dirname + "/Laughs/madara.mp3")}
+    api.sendMessage(sad, threadID, messageID);
+    api.setMessageReaction("😍", event.messageID, (err) => {}, true)
+        };
+}
+
+  module.exports.onStart = function({ api, event, client, __GLOBAL }) {}
