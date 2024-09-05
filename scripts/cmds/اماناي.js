4bddfe6ac7afc31c3e190d13d13f265c35835774
mouhamedalger <154691398+mouhamedalger@users.noreply.github.com>
2024-09-05 17:38:54 +0100

    Update and rename اعلام.js to اماناي.js

--- scripts/cmds/اماناي.js
+++ scripts/cmds/اماناي.js
@@ -1,11 +1,11 @@
 module.exports.config = {
-                name: "اعلام",
+                name: "اماناي",
          	version: "1.3",
 		author: "محمد تانجيرو",
-         	countDown: 5,
+         	countDown: 3,
             	role: 0,
-            	description: { ar: "ترسل ضحكات بعض الشخصيات الأسطورية" },
-	  	category: "no prefix",
+            	description: { ar: "إرسال صور أماناي من أنمي جوجوتسو كايسن" },
+	  	category: "pictures",
 	    	guide: { ar: "{pn}" }
                          };
 
@@ -14,12 +14,12 @@ const axios = require('axios');
 
 module.exports.onStart = async function ({ api, event, args }) {
     const pictures = [
-      { /*image:*/ "https://i.imgur.com/cuMzlKg.jpg"},
-      { /*image:*/ "https://i.imgur.com/2f52Onh.jpg"},
-      { /*image:*/ "https://i.imgur.com/B9zE4DA.jpg"},
-      { /*image:*/ "https://i.imgur.com/EZa7R9C.jpg"},
-      { /*image:*/ "https://i.imgur.com/j5FfpWE.jpg"},
-      { /*image:*/ "https://i.imgur.com/Wd3456M.jpg"},
+      { image: "https://i.imgur.com/cuMzlKg.jpg"},
+      { image: "https://i.imgur.com/2f52Onh.jpg"},
+      { image: "https://i.imgur.com/B9zE4DA.jpg"},
+      { image: "https://i.imgur.com/EZa7R9C.jpg"},
+      { image: "https://i.imgur.com/j5FfpWE.jpg"},
+      { image: "https://i.imgur.com/Wd3456M.jpg"},
 /*      { image: ""},
       { image: ""},
       { image: ""},
@@ -38,14 +38,14 @@ module.exports.onStart = async function ({ api, event, args }) {
       { image: ""},
       { image: ""},
       { image: ""},
-      { image: ""},*/    
+      { image: ""}, */   
     ];
 
     const randomimage = pictures[Math.floor(Math.random() * pictures.length)];
-    const image = await axios.get(randomimage/*.image*/, { responseType: "arraybuffer" });
+    const image = await axios.get(randomimage.image, { responseType: "arraybuffer" });
     fs.writeFileSync(__dirname + "/cache/amanai.jpg", Buffer.from(image.data, "binary"));
 
     const attachment = [fs.createReadStream(__dirname + "/cache/amanai.jpg")];
-    const message = `✨ لأي دولة ينتمي هذا العلم؟ ✨`;
+    const message = `✨        صور أماناي        💙\nمن أنمي جوجوتسو كايسن✨\n    🌹 عدد الصور : ${pictures.length} 🌹`;
     api.sendMessage({ body: message, attachment }, event.threadID, event.messageID);
 };
