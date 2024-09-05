7a403642d48c4d83e7f32096219bcf49ac18655f
⫷㊚ç𝖎𝖉×͜×𝖐𝖆𝖌𝖊𝖓𝖔㊚⫸ <sorokomarco@gmail.com>
2024-09-05 06:49:34 +0000

    Update callad.js

--- scripts/cmds/callad.js
+++ scripts/cmds/callad.js
@@ -37,13 +37,13 @@ module.exports = {
 			missingMessage: "Please enter the message you want to send to admin",
 			sendByGroup: "\n- Sent from group: %1\n- Thread ID: %2",
 			sendByUser: "\n- Sent from user",
-			content: "\n\nContent:\n─────────────────\n%1\n─────────────────\nReply this message to send message to user",
-			success: "Sent your message to %1 admin successfully!\n%2",
-			failed: "An error occurred while sending your message to %1 admin\n%2\nCheck console for more details",
-			reply: "📍 Reply from admin %1:\n─────────────────\n%2\n─────────────────\nReply this message to continue send message to admin",
-			replySuccess: "Sent your reply to admin successfully!",
-			feedback: "📝 Feedback from user %1:\n- User ID: %2%3\n\nContent:\n─────────────────\n%4\n─────────────────\nReply this message to send message to user",
-			replyUserSuccess: "Sent your reply to user successfully!",
+			content: "\n\nContent:\n✎﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏\n%1\n✎﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏\nReply this message to send message to user",
+			success: "𝙈𝙚𝙨𝙨𝙖𝙜𝙚 𝙩𝙧𝙖𝙣𝙨𝙛𝙚𝙧𝙚 𝙖 ✨%1✨ 𝙖𝙙𝙢𝙞𝙣(𝙨) 𝙖𝙫𝙚𝙘 𝙨𝙪𝙘𝙘𝙚𝙨✔ !\n%2",
+			failed: "𝐄𝐜𝐡𝐞𝐜 𝐝𝐞 𝐥'𝐞𝐧𝐯𝐨𝐢 𝐝𝐞 𝐯𝐨𝐭𝐫𝐞 𝐦𝐞𝐬𝐬𝐚𝐠𝐞 𝐚 ✨%1✨ 𝐚𝐝𝐦𝐢𝐧(𝐬)\n%2\n𝐌𝐨𝐧 𝐚𝐝𝐦𝐢𝐧 𝐯𝐞𝐫𝐢𝐟𝐢𝐞𝐫𝐚 𝐥𝐚 𝐜𝐨𝐧𝐬𝐨𝐥𝐞 𝐩𝐨𝐮𝐫 𝐫𝐞𝐠𝐥𝐞𝐫 𝐜𝐞 𝐝𝐞𝐭𝐚𝐢𝐥 𝐝𝐚𝐧𝐬 𝐥'𝐨𝐦𝐛𝐫𝐞💻 !",
+			reply: "🧬| 𝐑𝐞𝐩𝐨𝐧𝐬𝐞 𝐝𝐞 𝐥'𝐚𝐝𝐦𝐢𝐧 %1:\n✎﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏\n%2\n✎﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏\n𝐑𝐞𝐩𝐨𝐧𝐝𝐬😮‍💨 𝐚 𝐜𝐞 𝐦𝐞𝐬𝐬𝐚𝐠𝐞 𝐩𝐨𝐮𝐫 𝐜𝐨𝐧𝐭𝐢𝐧𝐮𝐞𝐫 𝐥𝐚 𝐜𝐨𝐧𝐯𝐞𝐫𝐬𝐚𝐭𝐢𝐨𝐧 𝐚𝐯𝐞𝐜 𝐦𝐨𝐧 𝐚𝐝𝐦𝐢𝐧♻️🇹🇬♻️",
+			replySuccess: "📩| 𝐌𝐨𝐧 𝐚𝐝𝐦𝐢𝐧 𝐫𝐞𝐜𝐞𝐯𝐫𝐚 𝐭𝐨𝐧 𝐦𝐞𝐬𝐬𝐚𝐠𝐞 𝐚 𝐥𝐚 𝐯𝐢𝐭𝐞𝐬𝐬𝐞 𝐝𝐞 𝐥'𝐨𝐦𝐛𝐫𝐞",
+			feedback: "🧬| 𝐌𝐞𝐬𝐬𝐚𝐠𝐞 𝐝𝐞 𝐥'𝐮𝐭𝐢𝐥𝐢𝐬𝐚𝐭𝐞𝐮𝐫 %1:\n- User ID: %2%3\n\nContent:\n✎﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏\n%4\n✎﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏\nReply this message to send message to user",
+			replyUserSuccess: "📩| 𝐌𝐞𝐬𝐬𝐚𝐠𝐞 𝐞𝐧𝐯𝐨𝐲𝐞 𝐚𝐯𝐞𝐜 𝐬𝐮𝐜𝐜𝐞𝐬♻️",
 			noAdmin: "Bot has no admin at the moment"
 		}
 	},
@@ -56,9 +56,9 @@ module.exports = {
 		if (config.adminBot.length == 0)
 			return message.reply(getLang("noAdmin"));
 		const senderName = await usersData.getName(senderID);
-		const msg = "==📨️ CALL ADMIN 📨️=="
-			+ `\n- User Name: ${senderName}`
-			+ `\n- User ID: ${senderID}`
+		const msg = "=♻️=📞📨️📱𝘾𝘼𝙇𝙇☎️𝑺𝑯𝑨𝑫𝑶𝑾×͜×📨️📞=♻️="
+			+ `\n- User Name: ✨🧬${senderName}🧬✨`
+			+ `\n- User ID: 🧬${senderID}🧬`
 			+ (isGroup ? getLang("sendByGroup", (await threadsData.get(threadID)).threadName, threadID) : getLang("sendByUser"));
 
 		const formMessage = {
