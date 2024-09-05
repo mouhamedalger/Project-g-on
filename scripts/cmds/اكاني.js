d1c0f891f20a73bbed62d69d1c145889b951f07e
mouhamedalger <154691398+mouhamedalger@users.noreply.github.com>
2024-09-05 20:28:10 +0100

    Update اكاني.js

--- scripts/cmds/اكاني.js
+++ scripts/cmds/اكاني.js
@@ -1,12 +1,12 @@
 module.exports.config = {
                 name: "اكاني",
               	version: "1.3",
-	            	author: "محمد تانجيرو",
+	        author: "محمد تانجيرو",
               	countDown: 3,
               	role: 0,
               	description: { ar: "إرسال صور أكاني من أنمي يامادا كن" },
-	            	category: "pictures",
-	            	guide: { ar: "{pn}" }
+	        category: "pictures",
+	        guide: { ar: "{pn}" }
                          };
 
 const fs = require('fs');
@@ -14,31 +14,31 @@ const axios = require('axios');
 
 module.exports.onStart = async function ({ api, event, args }) {
     const pictures = [
-      { image: "https://i.imgur.com/cuMzlKg.jpg" },
-      { image: "https://i.imgur.com/2f52Onh.jpg" },
-      { image: "https://i.imgur.com/B9zE4DA.jpg" },
-      { image: "https://i.imgur.com/EZa7R9C.jpg" },
-      { image: "https://i.imgur.com/j5FfpWE.jpg" },
-      { image: "https://i.imgur.com/Wd3456M.jpg" },
-      { image: "https://i.imgur.com/4LohdyV.jpg" },
-      { image: "https://i.imgur.com/H1yVPWV.jpg" },
-      { image: "https://i.imgur.com/go75jjw.jpg" },
-      { image: "https://i.imgur.com/3m0cEQs.jpg" },
-      { image: "https://i.imgur.com/UVA3ixP.jpg" },
-      { image: "https://i.imgur.com/in3Llrr.jpg" },
-      { image: "https://i.imgur.com/YLDQjiS.jpg" },
-      { image: "https://i.imgur.com/RoFtJSp.jpg" },
-      { image: "https://i.imgur.com/dNDy1NV.jpg" },
-      { image: "https://i.imgur.com/Hj32WtH.jpg" },
-      { image: "https://i.imgur.com/kHIloid.jpg" },
-      { image: "https://i.imgur.com/9ZcAkgT.jpg" },
-      { image: "https://i.imgur.com/bK1mhER.jpg" },
-      { image: "https://i.imgur.com/3JmZ1qt.jpg" },
-      { image: "https://i.imgur.com/5s2Vp0a.jpg" },
-      { image: "https://i.imgur.com/3sOAC6j.jpg" },
-      { image: "https://i.imgur.com/fzNkEfk.jpg" },
-      { image: "https://i.imgur.com/fhHIzTV.jpg" },
-      { image: "https://i.imgur.com/1WNPmFD.jpg" },  
+      { image: "https://i.imgur.com/mK2HpYC.jpeg" },
+      { image: "https://i.imgur.com/nxQcrJ1.jpeg" },
+      { image: "https://i.imgur.com/RQyWJo4.jpeg" },
+      { image: "https://i.imgur.com/jZsDRHY.jpeg" },
+      { image: "https://i.imgur.com/B2XKghY.jpeg" },
+      { image: "https://i.imgur.com/ewsX0tL.jpeg" },
+      { image: "https://i.imgur.com/EBodMQO.jpeg" },
+      { image: "https://i.imgur.com/HhV3uFy.jpeg" },
+      { image: "https://i.imgur.com/VwLDfBh.jpeg" },
+      { image: "https://i.imgur.com/tXUmKjT.jpeg" },
+      { image: "https://i.imgur.com/AOqZ6I6.jpeg" },
+      { image: "https://i.imgur.com/k2JVQLA.jpeg" },
+      { image: "https://i.imgur.com/0jQ0rSl.jpeg" },
+      { image: "https://i.imgur.com/qf396bJ.jpeg" },
+      { image: "https://i.imgur.com/SnnJq1m.jpeg" },
+      { image: "https://i.imgur.com/oJ5SANY.jpeg" },
+      { image: "https://i.imgur.com/TYrL3he.jpeg" },
+      { image: "https://i.imgur.com/ybesG9A.jpeg" },
+      { image: "https://i.imgur.com/jj4oVg0.jpeg" },
+      { image: "https://i.imgur.com/NTpOuni.jpeg" },
+      { image: "https://i.imgur.com/zMlHHbt.jpeg" },
+      { image: "https://i.imgur.com/VSylvCc.jpeg" },
+      { image: "https://i.imgur.com/WzLzeQx.jpeg" },
+      { image: "https://i.imgur.com/XiDqFWK.jpeg" },
+      { image: "https://i.imgur.com/DRHC7P5.jpeg" },
     ];
 
     const randomimage = pictures[Math.floor(Math.random() * pictures.length)];
@@ -46,6 +46,6 @@ module.exports.onStart = async function ({ api, event, args }) {
     fs.writeFileSync(__dirname + "/cache/akane.jpg", Buffer.from(image.data, "binary"));
 
     const attachment = [fs.createReadStream(__dirname + "/cache/akane.jpg")];
-    const message = `✨     صور أكاني كينوشيتا     💙\nمن أنمي يامادا كن✨\n    🌹 عدد الصور : ${pictures.length} 🌹`;
+    const message = `🌹 صور أكاني كينوشيتا 🌹\n   ♡_من أنمي يامادا كن_♡\n  ༺ا- عدد الصور : ${pictures.length} -ا༻`;
     api.sendMessage({ body: message, attachment }, event.threadID, event.messageID);
 };
