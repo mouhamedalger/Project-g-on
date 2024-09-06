43c4f467ec662d38d3ab59b23ee2ca1e19e68a79
mouhamedalger <154691398+mouhamedalger@users.noreply.github.com>
2024-09-06 10:03:38 +0100

    Update مطلوب.js

--- scripts/cmds/مطلوب.js
+++ scripts/cmds/مطلوب.js
@@ -4,14 +4,14 @@ module.exports.config = {
 		author: "محمد تانجيرو",
          	countDown: 3,
             	role: 0,
-            	description: { ar: "وضع صورتك أو صورة الي تعمل له تاغ في منشةر مطلوب" },
-	  	category: "pictures",
-	    	guide: { ar: "{pn} أو {pn} [@تاغ]" }
+            	description: { ar: "وضع صورتك، أو صورة الي تعمل له تاغ، أو صورة الي ترد عليه في منشور مطلوب" },
+	  	category: "edit",
+	    	guide: { ar: "{pn} أو {pn} [@تاغ] {pn} [رد]" }
                          };
 const fs = require('fs');
 const axios = require('axios');
 const { loadImage, createCanvas } = require('canvas');
-module.exports.onStart = async function ({ api, event, args, Users }) {
+module.exports.onStart = async function ({ api, event, args }) {
   let { senderID, threadID, messageID } = event;
   let pathImg = __dirname + "/cache/wanted.png";
   let pathAva = __dirname + "/cache/avatar.png";
@@ -19,17 +19,10 @@ module.exports.onStart = async function ({ api, event, args, Users }) {
   if(event.type == "message_reply") { uid = event.messageReply.senderID }
   if (args.join().indexOf('@') !== -1){ var uid = Object.keys(event.mentions) } 
   let Avatar = (
-    await axios.get(
-      `https://graph.facebook.com/${uid}/picture?height=1500&width=1500&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`,
-      { responseType: "arraybuffer" }
-    )
-  ).data;
+    await axios.get( `https://graph.facebook.com/${uid}/picture?height=1500&width=1500&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, { responseType: "arraybuffer" })).data;
   fs.writeFileSync(pathAva, Buffer.from(Avatar, "utf-8"));
   let getWanted = (
-    await axios.get(`https://i.imgur.com/QrYoP8F.jpeg`, {
-      responseType: "arraybuffer",
-    })
-  ).data;
+    await axios.get(`https://i.imgur.com/QrYoP8F.jpeg`, { responseType: "arraybuffer", })).data;
   fs.writeFileSync(pathImg, Buffer.from(getWanted, "utf-8"));
   let baseImage = await loadImage(pathImg);
   let baseAva = await loadImage(pathAva);
