af2c63874002e10c71caa7f2d926078ea5d9ca2c
mouhamedalger <154691398+mouhamedalger@users.noreply.github.com>
2024-09-06 10:29:05 +0100

    Create مشغول.js

---
+++ scripts/cmds/مشغول.js
@@ -0,0 +1,67 @@
+module.exports.config = {
+                name: "مشغول",
+		version: "1.3",
+            	author: "محمد تانجيرو",
+            	countDown: 5,
+	    	role: 0,
+            	description: { ar: "وضع مشغول، لمنع التاغات" },
+	    	category: "edit",
+    		guide: { ar: "{pn} [سبب الإنشغال]" }
+                         };
+
+const busyPath = __dirname + '/cache/busy.json';
+const fs = require('fs');
+
+module.exports.onLoad = () => {
+  if (!fs.existsSync(busyPath)) fs.writeFileSync(busyPath, JSON.stringify({}));
+}
+
+module.exports.onChat = async function({ api, event, Users }) {
+    let busyData = JSON.parse(fs.readFileSync(busyPath));
+    var { senderID, threadID, messageID, mentions } = event;
+    if (senderID in busyData) {
+        var info = busyData[senderID];
+        delete busyData[senderID];
+        fs.writeFileSync(busyPath, JSON.stringify(busyData, null, 4));
+        return api.sendMessage(`💙✨ مرحبا بعودتك ✨💙`, threadID, () => {
+            if (info.tag.length == 0) api.sendMessage("مافي أي شخص عملك تاغ 😌", threadID);
+            else {
+                var msg = "";
+                for (var i of info.tag) {
+                    msg += `${i}\n`
+                }
+                api.sendMessage("✨ أسماء الأعضاء الي عملوا لك تاغ، وعدد التاغات لكل عضو✨\n\n" + msg, threadID)
+            }
+        }, messageID);
+    }
+
+    if (!mentions || Object.keys(mentions).length == 0) return;
+    
+    for (const [ID, name] of Object.entries(mentions)) {
+        if (ID in busyData) {
+            var infoBusy = busyData[ID], mentioner = await Users.getNameUser(senderID), replaceName = event.body.replace(`${name}`, "");
+            infoBusy.tag.push(`${mentioner}: ${replaceName == "" ? "تاغ واحد فقط 😆" : replaceName}`)
+            busyData[ID] = infoBusy;
+            fs.writeFileSync(busyPath, JSON.stringify(busyData, null, 4));
+            return api.sendMessage(
+`${name.replace("@", "")} مشغول حاليا ${infoBusy.lido ? `✨السبب : ${infoBusy.lido}✨` : "."}`, threadID, messageID);
+        }
+    }
+}
+
+module.exports.onStart = async function({ api, event, args, Users }) {
+	await new Promise(resolve => setTimeout(resolve, 1000));
+    let busyData = JSON.parse(fs.readFileSync(busyPath));
+    const { threadID, senderID, messageID, body } = event;
+    var content = args.join(" ") || "";
+    if (!(senderID in busyData)) {
+        busyData[senderID] = {
+            lido: content,
+            tag: []
+        }
+        fs.writeFileSync(busyPath, JSON.stringify(busyData, null, 4));
+       var msg = (content.length == 0) ? '💙✨ أنت مشغول الآن ✨💙\n سبب الانشغال: لم يتم إدخاله\n\n✨ لما ترجع أعطيك الأعضاء الي عملوا لك تاغ في غيابك ☺️' : `💙✨ أنت مشغول الآن ✨💙\n سبب الانشغال: ${content}\n\n✨ لما ترجع أعطيك الأعضاء الي عملوا لك تاغ في غيابك ☺️`;
+        return api.sendMessage(msg, threadID, messageID);
+    }
+}
+
