0a417da4285648d338e583df3766b59a1f439872
⫷㊚ç𝖎𝖉×͜×𝖐𝖆𝖌𝖊𝖓𝖔㊚⫸ <sorokomarco@gmail.com>
2024-09-12 04:07:47 +0000

    Update prefix.js

--- scripts/cmds/prefix.js
+++ scripts/cmds/prefix.js
@@ -1,22 +1,31 @@
 module.exports = {
- config: {
-	 name: "prefix",
-	 version: "1.0",
-	 author: "Tokodori_Frtiz",//remodified by cliff
-	 countDown: 5,
-	 role: 0,
-	 shortDescription: "no prefix",
-	 longDescription: "no prefix",
-	 category: "system",
- },
+  config: {
+    name: "prefix",
+    version: "1.1",
+    author: "𝐜𝐢𝐝",
+    countDown: 5,
+    role: 0,
+    shortDescription: "Display bot prefix",
+    longDescription: "Shows the current command prefix of the bot.",
+    category: "system",
+  },
 
- onStart: async function(){}, 
- onChat: async function({ event, message, getLang }) {
- if (event.body && event.body.toLowerCase() === "prefix") {
- return message.reply({
- body: `『☣𝐏𝐑𝐄𝐅𝐈𝐗💀𝐒𝐇𝐀𝐃𝐎𝐖☣』:↠¥↞\n╔━━━━≛✞۝✞≛━━━━━╗\n㋰𝐀𝐓𝐎𝐌𝐈𝐂×͜×𝐒𝐘𝐒𝐓𝐄𝐌𝐄㋰:⇨¥⇦\n╚━━━━≛✞۝✞≛━━━━━╝\n☣𝑀𝑌 𝐶𝑅𝐸𝐴𝑇𝑂𝑅☣\n╔━≕≔≕≔✞≛❂≛✞≕≔≕≔━╗\n๖ۣ•҉✞𝐂𝐢𝐝×͜×𝐊𝐚𝐠𝐞𝐧𝐨✞๖ۣ•҉\n╚━≕≔≕≔✞≛❂≛✞≕≔≕≔━╝\n🥲✨『𝗧𝗮𝗽𝗲 ¥𝗵𝗲𝗹𝗽 𝗽𝗼𝘂𝗿 𝗮𝘃𝗼𝗶𝗿 𝗹𝗮 𝗹𝗶𝘀𝘁𝗲 𝗱𝗲 𝗰𝗼𝗺𝗺𝗮𝗻𝗱𝘀✨🥲』`,
- attachment: await global.utils.getStreamFromURL("https://i.ibb.co/PQQdjc8/image.jpg")
- });
- }
- }
-}
+  onStart: async function () {},
+
+  onChat: async function ({ event, message }) {
+    if (event.body && event.body.toLowerCase() === "prefix") {
+      return message.reply({
+        body: `🔥 ♫𝐏𝐑𝐄𝐅𝐈𝐗♫ 🔥\n
+╔═════≛✬✬≛═════╗\n
+𝐁𝐎𝐓 𝑃𝑅𝐸𝑓𝐼𝑆: ¥\n
+╚═════≛✬✬≛═════╝\n
+🛠 *𝐂𝐑𝐄𝐀𝐓𝐎𝐑* 🛠\n
+╔═════✹✹✹═════╗\n
+★𝑪𝑰𝑫×͜×𝐊𝐀𝐆𝐄𝐍𝐎✔\n
+╚═════✹✹✹═════╝\n
+💡 Tape "help" pour la liste des commandes 💡`,
+        attachment: await global.utils.getStreamFromURL("https://i.ibb.co/PQQdjc8/image.jpg")
+      });
+    }
+  }
+};
