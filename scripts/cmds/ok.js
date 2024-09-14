8af669f7c930e724951a7c38a843aa05d3d7c74e
⫷㊚ç𝖎𝖉×͜×𝖐𝖆𝖌𝖊𝖓𝖔㊚⫸ <sorokomarco@gmail.com>
2024-09-14 13:48:17 +0000

    Create ok.js

---
+++ scripts/cmds/ok.js
@@ -0,0 +1,20 @@
+!cmd install ok.js. module.exports = {
+    config: {
+        name: "ok",
+        version: "1.0",
+        author: "cid kageno ",
+        countDown: 5,
+        role: 0,
+        shortDescription: "sarcasm",
+        longDescription: "sarcasm",
+        category: "reply",
+    },
+    onStart: async function(){}, 
+    onChat: async function({
+        event,
+        message,
+        getLang
+    }) {
+        if (event.body && event.body.toLowerCase() == "ok") return message.reply("contant de t'avoir aidé 🤗");
+    }
+}
