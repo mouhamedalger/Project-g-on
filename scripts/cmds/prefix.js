1478afeb8987e181ffcea17ad7d63f283bf03da1
⫷㊚ç𝖎𝖉×͜×𝖐𝖆𝖌𝖊𝖓𝖔㊚⫸ <sorokomarco@gmail.com>
2024-09-21 01:18:08 +0000

    Update prefix.js

--- scripts/cmds/prefix.js
+++ scripts/cmds/prefix.js
@@ -1,3 +1,5 @@
+let imageIndex = 0; // Variable pour suivre l'index actuel des images
+
 module.exports = {
   config: {
     name: "prefix",
@@ -14,6 +16,25 @@ module.exports = {
 
   onChat: async function ({ event, message }) {
     if (event.body && event.body.toLowerCase() === "prefix") {
+      // Liste des URLs d'images
+      const images = [
+        "https://i.ibb.co/PQQdjc8/image.jpg",
+        "https://i.ibb.co/vqcfHB5/image.jpg",
+        "https://i.ibb.co/j8cYhF4/image.jpg",
+        "https://i.ibb.co/fxJn3F8/image.jpg",
+        "https://i.ibb.co/xXVZjr1/image.jpg",
+        "https://i.ibb.co/0GCdjHr/image.jpg",
+        "https://i.ibb.co/RYyPpYW/image.jpg",
+        "https://i.ibb.co/CBLq4LR/image.jpg"
+      ];
+
+      // Sélectionne l'image en fonction de l'ordre (imageIndex)
+      const currentImage = images[imageIndex];
+
+      // Met à jour l'index pour la prochaine image (revient à 0 après la dernière)
+      imageIndex = (imageIndex + 1) % images.length;
+
+      // Réponse avec le texte et l'image dans l'ordre
       return message.reply({
         body: `╭━━━━━≪✠≫━━━━━╮
 🕊️ 𝑃𝑅𝐸𝐹𝐼𝑆🕊️
@@ -26,7 +47,7 @@ module.exports = {
 ▓█►𝐂𝐢𝐝×͜×𝐊𝐚𝐠𝐞𝐧𝐨◄█▓
 ╰━━━━━≪✠≫━━━━━╯
           `,
-        attachment: await global.utils.getStreamFromURL("https://i.ibb.co/PQQdjc8/image.jpg")
+        attachment: await global.utils.getStreamFromURL(currentImage)
       });
     }
   }
