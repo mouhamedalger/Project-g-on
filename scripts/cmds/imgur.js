f21acd1e59bf23a01bd3cc1d4ed6051752c5e6d1
⫷㊚ç𝖎𝖉×͜×𝖐𝖆𝖌𝖊𝖓𝖔㊚⫸ <sorokomarco@gmail.com>
2024-09-23 00:11:12 +0000

    Update imgur.js

--- scripts/cmds/imgur.js
+++ scripts/cmds/imgur.js
@@ -1,50 +1,33 @@
 const axios = require('axios');
-const FormData = require('form-data');
-
-// Utilisez le token d'accès fourni directement
-const ACCESS_TOKEN = '5c3118ebb73fbb275945ab340be60b610a3216d6'; // Remplacez par votre propre access token
 
 module.exports = {
-    config: {
-        name: "imgur",
-        version: "1.0",
-        author: "Cid Kageno",
-        countDown: 5,
-        role: 0,
-        shortDescription: "Upload an image to Imgur",
-        longDescription: "Upload an image to Imgur",
-        category: "utility",
-        guide: "{pn} <attached image>"
-    },
-
-    onStart: async function({ message, event }) {
-        try {
-            const attachments = event.messageReply.attachments;
-            if (!attachments || attachments.length === 0) {
-                return message.reply("Please reply to a message with an attached image to upload.");
-            }
-
-            const imageUrl = attachments[0].url;
-            const form = new FormData();
-            form.append('image', imageUrl);
-
-            // Téléversez l'image sur Imgur en utilisant le token d'accès
-            const response = await axios.post('https://api.imgur.com/3/image', form, {
-                headers: {
-                    'Authorization': `Bearer ${ACCESS_TOKEN}`,
-                    ...form.getHeaders()
-                }
-            });
-
-            if (response.data && response.data.data && response.data.data.link) {
-                const imgurLink = response.data.data.link;
-                message.reply({ body: `🎁𝐢𝐦𝐚𝐠𝐞 𝐮𝐩𝐥𝐨𝐚𝐝💻 𝐛𝐲 𝐜𝐢𝐝×͜×☛⏳:${imgurLink}`});
-            } else {
-                message.reply("🙆🙆𝐨𝐨𝐩𝐬.");
-            }
-        } catch (error) {
-            console.error('Error:', error);
-            message.reply(`Error: ${error.message}`);
-        }
+  config: {
+    name: "imgur",
+    version: "1.0",
+    author: "otinxsandip",
+    countDown: 5,
+    role: 0,
+    longDescription: "Imgur link",
+    category: "image",
+    guide: {
+      en: "${pn} reply to image"
     }
+  },
+
+  onStart: async function ({ message, api, event }) {
+    const puti = event.messageReply?.attachments[0]?.url;
+
+    if (!puti) {
+      return message.reply('Please reply to an image.');
+    }
+
+    try {
+      const res = await axios.get(`https://sandipapi.onrender.com/imgur?link=${encodeURIComponent(puti)}`);
+      const lado = res.data.uploaded.image;
+      return message.reply(lado);
+    } catch (error) {
+      console.error(error);
+      return message.reply('api sucks bro.');
+    }
+  }
 };
